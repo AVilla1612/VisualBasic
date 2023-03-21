b3da94ad4b48712c9d9b62abfde7bf40e1128021
AVilla1612 <99267479+AVilla1612@users.noreply.github.com>
2023-03-21 14:12:41 +0100

    Add files via upload

---
+++ bucleDoLoop/bucleDoLoop/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim num As Integer
+
+        Do
+
+            Console.Write("Ingrese un número: ")
+            num = Console.ReadLine()
+
+            Console.WriteLine("Me encanta vb.net")
+
+
+        Loop While num <> 0
+
+    End Sub
+
+End Module
