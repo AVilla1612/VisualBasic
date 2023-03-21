0ebf20dc765282a92688623fd050d8ea659aa67e
AVilla1612 <99267479+AVilla1612@users.noreply.github.com>
2023-03-21 14:12:06 +0100

    Add files via upload

---
+++ arrays/arrays/Module1.vb
@@ -0,0 +1,29 @@
+﻿Module Module1
+
+    Sub Main()
+
+        'Dim nombres = New String() {"maria", "pedro", "juan", "daniela", "luis", "lucia"}'
+
+        Dim nombres(6) As String
+
+        nombres(0) = "Pedro"
+        nombres(1) = "Luis"
+        nombres(2) = "Juan"
+        nombres(3) = "Lucia"
+        nombres(4) = "Carmen"
+        nombres(5) = "Daniela"
+
+        For Each nom As String In nombres
+
+            Console.WriteLine(nom)
+
+
+
+        Next
+
+
+
+
+    End Sub
+
+End Module
