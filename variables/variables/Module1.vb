79855311549bec5c99be89bf719812d669a9e960
AVilla1612 <99267479+AVilla1612@users.noreply.github.com>
2023-03-21 14:19:51 +0100

    Add files via upload

---
+++ variables/variables/Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim num As Integer = 12
+
+        Dim pulgada As Double = 2.54
+
+        Dim letra As Char = "a"
+
+        Dim cadena As String = "My name is Antonio Villa"
+
+        Dim condicion As Boolean = True
+
+
+
+        Console.WriteLine(num)
+
+
+
+
+
+
+
+    End Sub
+
+End Module
