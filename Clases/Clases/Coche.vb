ea81078829d433a4477e0b93e74162c2109c315a
AVilla1612 <99267479+AVilla1612@users.noreply.github.com>
2023-03-21 15:35:11 +0100

    Add files via upload

---
+++ Clases/Clases/Coche.vb
@@ -0,0 +1,26 @@
+﻿Public Class Coche
+
+    Public marca As String
+    Public modelo As String
+    Public color As String
+    Public velocidad As Integer
+
+    Public Sub subirVelocidad()
+
+        velocidad += 10
+
+    End Sub
+
+    Public Sub bajarVelocidad()
+
+        velocidad -= 10
+
+    End Sub
+
+    Function velocidadActual() As Integer
+
+        Return velocidad
+
+    End Function
+
+End Class
