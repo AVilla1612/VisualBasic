d781e6d268c8cc7fef661b08cb0d68ac54eb18c4
AVilla1612 <99267479+AVilla1612@users.noreply.github.com>
2023-03-22 14:14:14 +0100

    Add files via upload

---
+++ DelegadoPredicadoLambda/DelegadoPredicadoLambda/Module1.vb
@@ -0,0 +1,41 @@
+﻿Module Module1
+
+    Sub Main()
+
+        'Dim persona1 As New Persona("Luis", 22)
+
+        'Dim encargado1 As New Encargado(AddressOf persona1.ToString)
+
+        'Console.WriteLine(encargado1())
+
+        Dim listaPersona As New List(Of Persona)
+
+        listaPersona.Add(New Persona("Luis", 22))
+        listaPersona.Add(New Persona("Carmen", 21))
+        listaPersona.Add(New Persona("Hugo", 19))
+        listaPersona.Add(New Persona("Lucia", 18))
+
+        'Dim objetoPredicado As New Predicate(Of Persona)(AddressOf MenoresVeinte)
+
+        Dim listaMenor20 As List(Of Persona) = listaPersona.FindAll(Function(p) p.EdadPersona < 20)
+
+    End Sub
+
+    'Delegate Function Encargado()
+
+    'Function MenoresVeinte(p As Persona) As Boolean
+
+    '    If p.EdadPersona < 20 Then
+
+    '        Return True
+
+    '    Else
+
+    '        Return False
+
+    '    End If
+
+    'End Function
+
+
+End Module
---
+++ DelegadoPredicadoLambda/DelegadoPredicadoLambda/Persona.vb
@@ -0,0 +1,35 @@
+﻿Public Class Persona
+
+    Private nombre As String
+    Private edad As Integer
+
+    Public Sub New(nombre As String, edad As Integer)
+        Me.nombre = nombre
+        Me.edad = edad
+    End Sub
+
+    Public Property NombrePersona() As String
+        Get
+            Return nombre
+        End Get
+        Set(value As String)
+            nombre = value
+        End Set
+    End Property
+
+    Public Property EdadPersona() As Integer
+        Get
+            Return edad
+        End Get
+        Set(value As Integer)
+            edad = value
+        End Set
+    End Property
+
+    Public Overrides Function ToString() As String
+
+        Return nombre & " " & edad
+
+    End Function
+
+End Class
